52056d5a25aa0a180fab7ff32859de7c0b30568a
Vladimir Gerasimenko <volodik1802@gmail.com>
2018-11-08 10:55:56 +0200

    Update google-map.js

--- src/plugins/google-map.js
+++ src/plugins/google-map.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import * as VueGoogleMaps from 'vue2-google-maps'
 
-const API_KEY = 'AIzaSyAVylLqwU7Q4ltUU2fMINJ7nW0I1cKiu2I'
+const API_KEY = 'AIzaSyBIuw3A7CHMlE57y-kcqFV7jQoqOsGsoe8'
 
 Vue.use(VueGoogleMaps, {
   load: {
